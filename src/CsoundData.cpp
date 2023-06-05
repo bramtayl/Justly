50e7cee424d01f68470149be09233e85054398ee
Brandon Taylor <brandon.taylor221@gmail.com>
2023-06-05 15:05:44 +0100

    fix

--- src/CsoundData.cpp
+++ src/CsoundData.cpp
@@ -2,79 +2,35 @@
 
 #include <QtCore/qtcoreexports.h>  // for qUtf8Printable
 #include <bits/chrono.h>           // for milliseconds
-#include <csound/csound.h>         // for csoundCompileOrc, csoundCreate
+#include <csound/csound.hpp>         // for csoundCompileOrc, csoundCreate
 #include <qbytearray.h>            // for QByteArray
 #include <qstring.h>               // for QString
 #include <QDebug>
 
-#include <thread>  // for sleep_for
-
-const auto LONG_TIME = std::chrono::milliseconds(10);
-const auto SHORT_TIME = std::chrono::nanoseconds(10);
-
-CsoundData::CsoundData()
-    : csound_object_pointer(csoundCreate(nullptr)),
-      thread_id(csoundCreateThread(csound_thread, (void *)this)){};
+CsoundData::CsoundData() {
+};
 
 CsoundData::~CsoundData() {
   stop_playing();
-  abort();
-  csoundJoinThread(thread_id);
-  csoundDestroy(csound_object_pointer);
 };
 
 void CsoundData::play(const QString &orchestra_text,
                             const QString &score_text) {
   stop_playing();
-
-  std::lock_guard<std::mutex> csound_lock(csound_mutex);
-  csoundSetOption(csound_object_pointer, "--output=devaudio");
-  csoundCompileOrc(csound_object_pointer, qUtf8Printable(orchestra_text));
-  csoundReadScore(csound_object_pointer, qUtf8Printable(score_text));
-  
-  should_play = true;
-  play_or_abort_signal.notify_one();
+  csound_object.Reset();
+  csound_object.SetOption("--output=devaudio");
+  csound_object.CompileOrc(qUtf8Printable(orchestra_text));
+  csound_object.ReadScore(qUtf8Printable(score_text));
+  csound_object.Start();
+  thread_pointer = new CsoundPerformanceThread(&csound_object);
+  thread_pointer -> Play();
 }
 
 void CsoundData::stop_playing() {
-  std::unique_lock<std::mutex> csound_lock(csound_mutex);
-  if (busy) {
-    should_play = false;
-    stop_signal.notify_one();
-    while (busy) {
-      play_or_abort_signal.wait(csound_lock);
+  if (thread_pointer != nullptr) {
+    if (thread_pointer -> GetStatus() == 0) {
+      thread_pointer -> Stop();
     }
+    thread_pointer->Join();
   }
 };
-
-void CsoundData::abort() {
-  std::lock_guard<std::mutex> csound_lock(csound_mutex);
-  should_run = false;
-  play_or_abort_signal.notify_one();
-};
-
-void CsoundData::run_backend() {
-  std::unique_lock<std::mutex> csound_lock(csound_mutex);
-  while (should_run) {
-    if (should_play) {
-      busy = true;
-      csoundStart(csound_object_pointer);
-      while (should_play) {
-        if (csoundPerformKsmps(csound_object_pointer) != 0) {
-          should_play = false;
-          break;
-        }
-        stop_signal.wait_for(csound_lock, SHORT_TIME);
-      }
-      csoundReset(csound_object_pointer);
-      busy = false;
-      play_or_abort_signal.notify_one();
-    }
-    play_or_abort_signal.wait_for(csound_lock, LONG_TIME);
-  }
-}
-
-auto csound_thread(void *csound_data_pointer) -> uintptr_t {
-  (static_cast<CsoundData *>(csound_data_pointer))->run_backend();
-  return 0;
-}
--- src/CsoundData.h
+++ src/CsoundData.h
@@ -1,29 +1,16 @@
 #pragma once
 
-#include <csound/csound.h>  // for CSOUND
+#include <csound/csound.hpp>  // for CSOUND
+#include <csound/csPerfThread.hpp>
 #include <qstring.h>        // for QString
-#include <stdint.h>         // for uintptr_t
-#include <mutex>
-#include <condition_variable>
-
-uintptr_t csound_thread(void *csound_data_pointer);
 
 class CsoundData {
  public:
-  CSOUND *const csound_object_pointer;
-  bool is_running = false;
-  bool should_run = true;
-  bool should_play = false;
-  bool busy = false;
-  void *const thread_id;
-  std::mutex csound_mutex;
-  std::condition_variable stop_signal;
-  std::condition_variable play_or_abort_signal;
+  Csound csound_object;
+  CsoundPerformanceThread *thread_pointer = nullptr;
   
   explicit CsoundData();
   ~CsoundData();
   void play(const QString &orchestra_text, const QString &score_text);
   void stop_playing();
-  void abort();
-  void run_backend();
 };
