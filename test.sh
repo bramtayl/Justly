1c0399e59fedab9affa1112dc6feb0f4b75066c7
Brandon Taylor <brandon.taylor221@gmail.com>
2023-08-07 19:37:32 +0100

    fix github actions?

--- test.sh
+++ test.sh
@@ -1,2 +1,10 @@
+mkdir "Release"
+cmake -S "." -B "./Release" -DCMAKE_BUILD_TYPE="Release" -DCMAKE_TOOLCHAIN_FILE="~/vcpkg/scripts/buildsystems/vcpkg.cmake"
+cmake --build "./Release" --config "Release" --target "Justly"
+mkdir "Debug"
+cmake -S "." -B "./Debug" -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_TOOLCHAIN_FILE="~/vcpkg/scripts/buildsystems/vcpkg.cmake"
+cmake --build "./Debug" --config "Debug" --target "Justly"
+cpack --config "MultiCpackConfig.cmake"
+
 lcov --capture --directory build --output-file coverage/lcov.info
 lcov --extract coverage/lcov.info '/home/brandon/Justly/src/*' --output-file coverage/lcov.info
