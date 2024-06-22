3ce9d35958fd9c902a5b4148d1f9e6ccad263cdf
Brandon Taylor <brandon.taylor221@gmail.com>
2024-06-22 09:21:56 +0100

    turn off track coverage

--- generate_coverage.sh
+++ generate_coverage.sh
@@ -1,8 +1,4 @@
 cmake -S . -B build -DBUILD_TESTS=ON -DTRACK_COVERAGE=ON
 cmake --build build
-#lcov --capture --initial --directory build --include "$(pwd)/**" --exclude "$(pwd)/build/**" --output-file coverage/uncovered.info
 ctest --test-dir build
 gcovr --exclude "build/.*" --html-nested coverage/coverage.html
-# lcov --capture --directory build --include "$(pwd)/**" --exclude "$(pwd)/build/**" --output-file coverage/covered.info
-# lcov --add-tracefile coverage/uncovered.info --add-tracefile coverage/covered.info --output-file coverage/lcov.info
-# genhtml coverage/lcov.info --output-directory coverage/report
