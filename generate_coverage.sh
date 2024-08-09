60275bb49db8a4ddaca0bfd256c04f3dcf710819
Brandon Taylor <brandon.taylor221@gmail.com>
2024-08-09 17:35:03 +0100

    add gcov filter

--- generate_coverage.sh
+++ generate_coverage.sh
@@ -1,3 +1,3 @@
 # use gcc not clang
 # make sure BUILD_TESTS=ON and TRACK_COVERAGE=ON
-gcovr --exclude "build/.*" --xml coverage/coverage.xml
+gcovr --exclude "$(pwd)/src/" --xml coverage/coverage.xml
