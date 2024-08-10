1909a0467e4ee62ec51294146b9e6b405f39103b
Brandon Taylor <brandon.taylor221@gmail.com>
2024-08-10 11:03:37 +0100

    add icon

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,10 +1,23 @@
 #include <QApplication>
+#include <QCoreApplication>
+#include <QDir>
+#include <QFile>
+#include <QIcon>
+#include <QtGlobal>
 
 #include "justly/SongEditor.hpp"
 
 auto main(int number_of_arguments, char *arguments[]) -> int {
   QApplication const app(number_of_arguments, arguments);
   QApplication::setApplicationDisplayName("Justly");
+
+  auto icon_file = QDir(QCoreApplication::applicationDirPath())
+                       .filePath("../share/Justly.svg");
+  Q_ASSERT(QFile::exists(icon_file));
+  QIcon icon(icon_file);
+  Q_ASSERT(!icon.isNull());
+  QApplication::setWindowIcon(icon);
+
   register_converters();
   SongEditor song_editor;
   song_editor.show();
